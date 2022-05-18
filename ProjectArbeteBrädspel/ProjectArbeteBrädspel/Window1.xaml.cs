583159cb27a4c05a56ffb0a22929ab75b50172fc
Exafyll <letrjo01@gmail.com>
2022-05-18 12:11:53 +0200

    Ignore

---
+++ ProjectArbeteBrädspel/ProjectArbeteBrädspel/Window1.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProjectArbeteBrädspel
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+
+
+            var doc = XElement.Parse(tempXml);
+            var target = doc.Elements("Project")
+                    .Where(e => e.Attribute("ID").Value == "2")
+                    .Single();
+
+            target.Attribute("Name").Value = "Project2_Update";
+            doc.Save(Console.Out);
+            Console.WriteLine();
+
+        }
+    }
+}
